b44c3493fe1e4f6172a4ba714153f83e3bf19c93
오다혜 <fffvvv6@gmail.com>
2023-04-06 00:27:37 +0900

    feat: Next.js 초기 세팅

---
+++ src/pages/_document.tsx
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="ko">
+      <Head>
+          <title>AI chat service</title>
+          <meta name="description" content="Next.js AI chat service" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+const Home = () => {
+  return (
+      <main>
+          <div>HOME</div>
+      </main>
+  )
+}
+
+export default Home;
