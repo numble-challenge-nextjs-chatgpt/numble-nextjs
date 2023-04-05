1bea6f4b29af4654ff6380f57b451b5498e95b0e
오다혜 <fffvvv6@gmail.com>
2023-04-06 00:55:16 +0900

    chore: prettier 설치 및 적용

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
-
 const Home = () => {
   return (
-      <main>
-          <div>HOME</div>
-      </main>
-  )
-}
+    <main>
+      <div>HOME</div>
+    </main>
+  );
+};
 
 export default Home;
