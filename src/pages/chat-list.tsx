e0d5cfb392b195a721dba1d22f555a156043b00d
오다혜 <fffvvv6@gmail.com>
2023-04-08 17:50:26 +0900

    style: 공통 구조 스타일링 수정

--- src/pages/chat-list.tsx
+++ src/pages/chat-list.tsx
@@ -1,3 +1,4 @@
+import CommonButton from '@/components/common/CommonButton';
 import PageContainer from '@/components/common/PageContainer';
 import Image from 'next/image';
 import styles from './chat-list.module.css';
@@ -10,16 +11,20 @@ const ChatList = () => {
     <PageContainer>
       <div className={styles['page-container']}>
         <header className={styles['header']}>
-          <Image src={logoImg} alt={'numble logo'} />
-          <Image src={plusIcon} alt={'plus icon to add chat room'} />
+          <Image src={logoImg} alt={'numble logo'} height={20} />
+          <Image
+            src={plusIcon}
+            alt={'plus icon to add chat room'}
+            height={26}
+          />
         </header>
         <div style={{ overflow: 'scroll' }}>
           {Array.from({ length: 1000 }).map((_, index) => (
             <div>채팅 리스트</div>
           ))}
         </div>
-        <div>
-          <button>버튼!</button>
+        <div className={styles['button-container']}>
+          <CommonButton text={'방 생성'} />
         </div>
       </div>
     </PageContainer>
