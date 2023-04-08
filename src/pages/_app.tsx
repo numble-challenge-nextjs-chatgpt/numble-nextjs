7d537b649c434e662dacfa9765238b5404d035de
오다혜 <fffvvv6@gmail.com>
2023-04-08 13:29:18 +0900

    style: css 변수 추가, 파비콘 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,7 +10,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <title>AI chat service</title>
         <meta name="description" content="Next.js AI chat service" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
       </Head>
       <Component {...pageProps} />
     </>
