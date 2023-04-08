47b2dbc7c7446df88bd01279f6839e42f214d44c
오다혜 <fffvvv6@gmail.com>
2023-04-08 19:15:54 +0900

    feat: CommonModal, CreateChatModal 생성

--- src/pages/chat-list.tsx
+++ src/pages/chat-list.tsx
@@ -1,3 +1,7 @@
+import CreateChatModal from '@/components/modals/CreateChatModal';
+import { useState } from 'react';
+import { createPortal } from 'react-dom';
+
 import CommonButton from '@/components/common/CommonButton';
 import PageContainer from '@/components/common/PageContainer';
 import Image from 'next/image';
@@ -7,16 +11,23 @@ import logoImg from '/public/images/numble-logo2.png';
 import plusIcon from '/public/icons/plus-icon.png';
 
 const ChatList = () => {
+  const [openModal, setOpenModal] = useState(false);
+  const openCreateChatRoomModal = () => {
+    setOpenModal(true);
+  };
+
   return (
     <PageContainer>
       <div className={styles['page-container']}>
         <header className={styles['header']}>
           <Image src={logoImg} alt={'numble logo'} height={20} />
-          <Image
-            src={plusIcon}
-            alt={'plus icon to add chat room'}
-            height={26}
-          />
+          <button onClick={openCreateChatRoomModal}>
+            <Image
+              src={plusIcon}
+              alt={'plus icon to add chat room'}
+              height={26}
+            />
+          </button>
         </header>
         <div style={{ overflow: 'scroll' }}>
           {Array.from({ length: 1000 }).map((_, index) => (
@@ -27,6 +38,15 @@ const ChatList = () => {
           <CommonButton text={'방 생성'} />
         </div>
       </div>
+      {openModal &&
+        createPortal(
+          <CreateChatModal
+            onClose={() => {
+              setOpenModal(false);
+            }}
+          />,
+          document.body,
+        )}
     </PageContainer>
   );
 };
