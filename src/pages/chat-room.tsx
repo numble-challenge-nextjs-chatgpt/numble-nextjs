46191517cc93f0c7c9acdd2693b5adde34e60f8f
오다혜 <fffvvv6@gmail.com>
2023-04-15 20:41:38 +0900

    feat: openai 라이브러리 설치, 연결

--- src/pages/chat-room.tsx
+++ src/pages/chat-room.tsx
@@ -1,3 +1,25 @@
+import { GetServerSideProps, GetServerSidePropsContext } from 'next';
+import { Configuration, OpenAIApi } from 'openai';
+
+export const getServerSideProps = async ({
+  req,
+  res,
+}: GetServerSidePropsContext) => {
+  const configuration = new Configuration({
+    organization: 'org-984ygV87ZSpNwE6xBGJz2Yrr',
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+
+  const openai = new OpenAIApi(configuration);
+  const response = await openai.listEngines();
+
+  console.log(response);
+
+  return {
+    props: {}, // will be passed to the page component as props
+  };
+};
+
 const ChatRoom = () => {
   return (
     <div>
